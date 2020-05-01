906a279773ac09640a1249b7da51b47f23de756e
citizen-dror <reshef.dror@gmail.com>
2020-05-01 12:09:42 +0300

    basic map with location

--- App.js
+++ App.js
@@ -4,11 +4,19 @@ import Constants from 'expo-constants';
 import * as Location from 'expo-location';
 import MapView from 'react-native-maps';
 
+const INIT_REGION = {
+  latitude: 31.728371,
+  longitude: 35.040161,
+  latitudeDelta: 1,
+  longitudeDelta: 1,
+}
+
 export default function App() {
   const [locationStatus, setLocationStatus] = useState(false);
   const [textLocation, setTextLocation] = useState('');
   const [errorMsg, setErrorMsg] = useState(null);
   const [location, setLocation] = useState(null);
+  const [region, setRegion] = useState(INIT_REGION);
 
   const textStatus = (locationStatus) ? 'Getting Location' : 'No Location';
   const textButton = (locationStatus) ? 'Stop Locating' : 'Start Locating';
@@ -25,10 +33,19 @@ export default function App() {
         }
         let location = await Location.getCurrentPositionAsync({});
         setLocation(location);
+        const newRegion = {
+          latitude: location.coords.latitude,
+          longitude: location.coords.longitude,
+          latitudeDelta: 0.01,
+          longitudeDelta: 0.01
+        };
+        setRegion(newRegion);
       })();
     }
   }
-
+  const onRegionChange = (region) => {
+    setRegion(region);
+  }
   const setMyLocation = () => {
     setLocationStatus(!locationStatus);
     getLoaction1();
@@ -45,11 +62,14 @@ export default function App() {
       <Text key='location'>{text}</Text>
       <Button title={textButton}
         onPress={() => { setMyLocation() }} />
-      <MapView style={styles.mapStyle} />
+      <MapView
+        style={styles.mapStyle}
+        region={region}
+        onRegionChange={(region) => onRegionChange(region )}
+      />
     </View>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -59,6 +79,6 @@ const styles = StyleSheet.create({
   },
   mapStyle: {
     width: Dimensions.get('window').width,
-    height: Dimensions.get('window').height *0.7,
+    height: Dimensions.get('window').height * 0.7,
   },
 });
