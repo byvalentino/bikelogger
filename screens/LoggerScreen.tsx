868cd555aaf8cc996130ce1f20252402810101a7
citizen-dror <reshef.dror@gmail.com>
2020-08-13 13:40:22 +0300

    add data to loggerScreen

--- screens/LoggerScreen.tsx
+++ screens/LoggerScreen.tsx
@@ -1,92 +1,88 @@
 //@ts-nocheck
-import React, { FC, useState } from 'react';
-import { StyleSheet, SafeAreaView, FlatList, Text, View, TouchableOpacity, Button } from 'react-native';
-import { inject, observer } from 'mobx-react';
-import { useNavigation } from '@react-navigation/native';
+import React, { Component } from 'react';
+import { Text, View, FlatList, Dimensions, Button, StyleSheet } from 'react-native';
 import { logHistory } from '../services/Logger';
-import Colors from '../constants/colors';
 
-export interface Props {
-  store?: any;
-}
-const DATA = [
-  {
-    id: "bd7acbea-c1b1-46c2-aed5-3ad53abb28ba",
-    title: "First Item",
-  },
-  {
-    id: "3ac68afc-c605-48d3-a4f8-fbd91aa97f63",
-    title: "Second Item",
-  },
-  {
-    id: "58694a0f-3da1-471f-bd96-145571e29d72",
-    title: "Third Item",
-  },
-];
+const { width } = Dimensions.get('window');
 
-const Item = ({ item, onPress, style }) => (
-  <TouchableOpacity onPress={onPress} style={[styles.item, style]}>
-    <Text style={styles.title}>{item.title}</Text>
-  </TouchableOpacity>
-);
+const style = {
+  justifyContent: 'center',
+  alignItems: 'center',
+  width: width,
+  height: 50,
+  flex: 1,
+  borderWidth: 1,
+};
 
-const LoggerScreen: FC<Props> = (props: Props ) => {
-  // const { loggerModalVisable, setLoggerModalVisable } = props.store;
-  const [selectedId, setSelectedId] = useState(null);
-  const navigation = useNavigation();
-  //const list = logHistory.map((x, index) => <Text style={styles.line} key={`line-${index}`} >{x}</Text>);
-  //const listItems = logHistory.map((x, index) => <li key={`line-${index}`}><Text >{x}</Text></li>);
+const COLORS = ['#FBFCFC','#AED6F1'];
 
-  const closeMethod =() =>{
-    navigation.goBack();
+class ScrollFlatList extends Component {
+  size = Math.min([this.props.data.length,50]);
+  getItemLayout = (data, index) => (
+    { length: 50, offset: 50 * index, index }
+  )
+  getColor(index) {
+    const mod = index%2;
+    return COLORS[mod];
   }
-  const renderItem = ({ item }) => {
-    const backgroundColor = item.id === selectedId ? "#6e3b6e" : "#f9c2ff";
+  scrollToIndex = () => {
+    let randomIndex = Math.floor(Math.random(Date.now()) * this.props.data.length);
+    this.flatListRef.scrollToIndex({animated: true, index: randomIndex});
+  }
+  scrollToItem = () => {
+    let randomIndex = Math.floor(Math.random(Date.now()) * this.props.data.length);
+    this.flatListRef.scrollToIndex({animated: true, index: "" + randomIndex});
+  }
+
+  render() {
     return (
-      <Item
-        item={item}
-        onPress={() => setSelectedId(item.id)}
-        style={{ backgroundColor }}
-      />
-    );
-  };
-
-  return (
-    <View>
-      <Text>Log:</Text>
-      <SafeAreaView style={styles.container}>
+      <View style={styles.container}>
+        <View style={styles.header}>
+          <Button
+            onPress={this.scrollToIndex}
+            title="scroll To Index"
+            color="darkblue"
+          />
+          <Button
+            onPress={this.scrollToItem}
+            title="scroll To Item"
+            color="purple"
+          />
+        </View>
         <FlatList
-          data={DATA}
-          renderItem={renderItem}
+          style={{ flex: 1 }}
+          ref={(ref) => { this.flatListRef = ref; }}
           keyExtractor={(item) => item.id}
-          extraData={selectedId}
+          getItemLayout={this.getItemLayout}
+          initialScrollIndex={this.size}
+          initialNumToRender={2}
+          renderItem={({ item, index}) => (
+              <View style={{...style, backgroundColor: this.getColor(index)}}>
+                <Text>{item.msg}</Text>
+              </View>
+            )}
+          {...this.props}
         />
-      </SafeAreaView>
-      <View style={styles.buttonClose}>
-        <Button title="Close" color={Colors.primary} onPress={closeMethod} />
       </View>
-    </View>
-
-  );
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    margin: 20,
+    flex: 1
   },
-  item: {
-    backgroundColor: '#f9c2ff',
-    padding: 20,
-    marginVertical: 8,
-    marginHorizontal: 16,
-  },
-  title: {
-    fontSize: 32,
-  },
-  buttonClose: {
-    alignItems: "center",
-    width: 100,
-  },
+  header: {
+    paddingTop: 20,
+    backgroundColor: 'darkturquoise',
+    justifyContent: 'flex-start', 
+  }
 });
-export default inject("store")(observer(LoggerScreen));
+
+export default class LoggerScreen extends Component {
+  render() {
+    return  <ScrollFlatList
+               data={logHistory}
+             />
+  }
+}
